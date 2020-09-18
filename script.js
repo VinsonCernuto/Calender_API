ef87ad2dd8e6c8b9ef302350f31ef2a8c3322bc7
Vinson Cernuto <cernutovinson@gmail.com>
2020-09-18 17:38:47 -0400

    created functions for local storage

--- script.js
+++ script.js
@@ -1,10 +1,99 @@
-// 1.- Display current time using Moment.js
-console.log(moment().format("dddd MMMM Do"));
-var now = moment().format("dddd MMMM Do");
-$("#currentDay").text(now);
-// 2.- Click event to save text area content to Local Storage
+//global var for loop
+var dayEl = [
+    {
+        id: "0",
+        hour: "09",
+        time: "am",
+        meridiem: "am",
+        reminder: ""
+    },
+    {
+        id: "1",
+        hour: "10",
+        time: "10",
+        meridiem: "am",
+        reminder: ""
+    },
+    {
+        id: "2",
+        hour: "11",
+        time: "11",
+        meridiem: "am",
+        reminder: ""
+    },
+    {
+        id: "3",
+        hour: "12",
+        time: "12",
+        meridiem: "pm",
+        reminder: ""
+    },
+    {
+        id: "4",
+        hour: "01",
+        time: "13",
+        meridiem: "pm",
+        reminder: ""
+    },
+    {
+        id: "5",
+        hour: "02",
+        time: "14",
+        meridiem: "pm",
+        reminder: ""
+    },
+    {
+        id: "6",
+        hour: "03",
+        time: "15",
+        meridiem: "pm",
+        reminder: ""
+    },
+    {
+        id: "7",
+        hour: "04",
+        time: "16",
+        meridiem: "pm",
+        reminder: ""
+    },
+    {
+        id: "8",
+        hour: "05",
+        time: "17",
+        meridiem: "pm",
+        reminder: ""
+    },
+]
+// - Display current time using Moment.js
+currentTime();
 
-// 3.- When app opens, display content for each time block from local storage.
+function currentTime() {
+    var currentTime = moment().format("dddd MMMM Do");
+    $("#currentDay").text(currentTime);
+}
+// - Click event to save text area content to Local Storage
 
-// 4.- Change time block background color depending in the current hour (past, present and future).
+//save data to localStorage
+function saveInfo(){
+    localStorage.setItem("dayEl", JSON.stringify(dayEl));
+}
+
+//sets data in the store to the text box
+function displayInfo(){
+    dayEl.forEach(function (_thishour) {
+        $("#${_thisHour.id}").val(_thishour.reminder);
+    })
+}
+
+//set local storage to view
+function init(){
+    var stored = JSON.parse(localStorage.getItem("dayEl"));
+
+    if (storedDay) {
+        dayEl = storedDay;
+    }
+    saveReminder();
+    displayReminder();
+}
+// - Change time block background color depending in the current hour (past, present and future).
 console.log(moment().hour())
